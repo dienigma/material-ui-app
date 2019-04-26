215b22f93a193a48a49b8851b24f8526a00f708a
dienigma <joshi.chinmay09@gmail.com>
2019-04-26 16:45:07 +0530

    Updated the data store

--- src/store.js
+++ src/store.js
@@ -17,31 +17,35 @@ export const exercises = [
   {
     id: "barbell-curls",
     title: "Barbell Curls",
-    description: "Biceps exercise...",
+    description:
+      "Barbell Curl Instructions. Stand up with your torso upright while holding a barbell at a shoulder-width grip. The palm of your hands should be facing forward and the elbows should be close to the torso. Continue the movement until your biceps are fully contracted and the bar is at shoulder level.",
     muscles: "arms"
   },
   {
     id: "bench-press",
     title: "Bench Press",
-    description: "Chest exercise...",
+    description: `The bench press is an upper-body strength-training exercise that consists of pressing a weight upwards from a supine position. The exercise works the pectoralis major as well as the supporting chest, arm, and shoulder muscles such as the anterior deltoids, serratus anterior, coracobrachialis, scapulae fixers, trapezii, and the triceps. A barbell is generally used to hold the weight, but a pair of dumbbells can also be used.[1]
+The barbell bench press is one of three lifts in the sport of powerlifting (alongside the deadlift and squat) and is the only lift in the sport of Paralympic powerlifting. It is also used extensively in weight training, bodybuilding, and other types of training to develop the chest muscles.`,
     muscles: "chest"
   },
   {
     id: "pull-ups",
     title: "Pull Ups",
-    description: "Back and biceps exercise...",
+    description:
+      "A pull-up is an upper-body compound pulling exercise. Although it can be performed with any grip, in recent years some have used the term to refer more specifically to a pull-up performed with a palms-forward position.",
     muscles: "back"
   },
   {
     id: "deadlifts",
     title: "Deadlifts",
-    description: "Back and leg exercise...",
+    description: `Deadlift refers to the lifting of dead weight (weight without momentum), such as weights lying on the ground.`,
     muscles: "back"
   },
   {
     id: "squats",
     title: "Squats",
-    description: "Legs exercise...",
+    description:
+      "In strength training and fitness, the squat is a compound, full-body exercise that trains primarily the muscles of the thighs, hips and buttocks, quadriceps femoris muscle, hamstrings, as well as strengthening the bones, ligaments and insertion of the tendons throughout the lower body.",
     muscles: "legs"
   }
 ];
